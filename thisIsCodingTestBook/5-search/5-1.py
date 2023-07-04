83f0daee9876097fe5d020585a945ac43352d194
sunny5875 <sunny5875@hanyang.ac.kr>
2023-07-04 22:39:37 +0900

    study dfs,bfs

---
+++ thisIsCodingTestBook/5-search/5-1.py
@@ -0,0 +1,11 @@
+# stack
+stack = []
+stack.append(4)
+stack.append(3)
+stack.pop() #3
+#queue
+from collections import deque
+queue = deque()
+queue.append(4)
+queue.append(3)
+queue.popleft() #4
---
+++ thisIsCodingTestBook/5-search/dfs.py
@@ -0,0 +1,23 @@
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end = ' ')
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+# 인접 리스트로 표현한 그래
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+# 방문한 노드 저장하는 배열
+visited = [False] * 9
+
+dfs(graph, 1, visited)
